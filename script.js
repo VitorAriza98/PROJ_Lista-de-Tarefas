99be782877b9f770d6c6fef213bb1bcb77bd8b63
VitroAriza <vitorariza@outlook.com>
2021-11-04 20:58:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const elementolista = document.querySelector('ul')
+const elementoInput = document.querySelector('input')
+const elementoBotao = document.querySelector('button')
+
+const tarefas = [];
+
+
+function mostrarTarefas() {
+
+    elementolista.innerHTML= ''
+
+    for (tarefa of tarefas) {
+      const elementoTarefa = document.createElement('li')
+      const textoTarefa = document.createTextNode(tarefa)
+
+      
+      const elementoLink = document.createElement('a')
+      const pos = tarefas.indexOf(tarefa)
+
+      const linkText = document.createTextNode('X')
+      elementoLink.appendChild(linkText)      
+
+      elementoLink.setAttribute('href', '#')    
+
+      elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`)
+
+      elementoTarefa.appendChild(textoTarefa)
+      elementolista.appendChild(elementoTarefa)
+      elementoTarefa.appendChild(elementoLink)
+
+    }
+}
+mostrarTarefas()
+
+function addTarefa() {
+    const textoTarefa = elementoInput.value
+    tarefas.push(textoTarefa)
+    elementoInput.value = ''
+
+    mostrarTarefas()
+}
+
+elementoBotao.setAttribute('onclick', 'addTarefa()')
+
+function deletaTarefa(pos) {
+    tarefas.splice(pos, 1)
+    mostrarTarefas()
+}
+
+
+
+
+
+
+
+
